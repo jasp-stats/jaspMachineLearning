edcff6b6ddfdde678024df3ab73087ed2cba9b3e
Koen Derks <koen-derks@hotmail.com>
2024-09-11 00:26:13 +0200

    Update test-mlclassificationlogisticmultinomial.R

--- tests/testthat/test-mlclassificationlogisticmultinomial.R
+++ tests/testthat/test-mlclassificationlogisticmultinomial.R
@@ -49,10 +49,32 @@ test_that("Confusion Matrix table results match", {
 			 "virginica", 0, 0, 12))
 })
 
+test_that("Feature Importance Metrics table results match", {
+	table <- results[["results"]][["featureImportanceTable"]][["data"]]
+	jaspTools::expect_equal_tables(table,
+		list(530.419652531233, "Petal.Length", 258.101247632355, "Petal.Width",
+			 11.6632506085855, "Sepal.Width", 10.7849556008181, "Sepal.Length"
+			))
+})
+
 test_that("Data Split plot matches", {
 	plotName <- results[["results"]][["plotDataSplit"]][["data"]]
 	testPlot <- results[["state"]][["figures"]][[plotName]][["obj"]]
 	jaspTools::expect_equal_plots(testPlot, "data-split")
+})
+
+test_that("Additive Explanations for Predictions of Test Set Cases table results match", {
+	table <- results[["results"]][["tableShap"]][["data"]]
+	jaspTools::expect_equal_tables(table,
+		list(0.629688901507271, 0.0369777616411127, 2.01684224876431e-11, 3.29787464004028e-09,
+			 0.333333333528117, 1, "setosa (1)", 0.608365336494419, 0.0583013168725895,
+			 5.75901548671709e-11, 1.30466177861166e-08, 0.333333333528117,
+			 2, "setosa (1)", 0.578930644244146, 0.0877359705540646, -1.91210501876427e-08,
+			 6.96919006948349e-08, 0.333333333528117, 3, "setosa (1)", 0.644504774615733,
+			 0.0221618748599225, 2.48312481687663e-11, 1.69711121822402e-08,
+			 0.333333333528117, 4, "setosa (1)", 0.544084722419656, 0.122581029133867,
+			 2.34213792804638e-09, 9.1257324152938e-07, 0.333333333528117,
+			 5, "setosa (1)"))
 })
 
 test_that("Model Performance Metrics table results match", {
